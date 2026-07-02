6b17d44e378cadf94d2a6a194745d08f42f08219
Araj Sahi <arajsahi46@gmail.com>
2026-07-02 13:34:02 -0400

    Addd multi-niche and dublicate removal

--- outreach.py
+++ outreach.py
@@ -5,31 +5,52 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 import csv
+NICHES = [
+    "productivity",
+    "personal finance",
+    "entrepreneurship",
+    "self improvement",
+    "tech tutorials"
+
+
+
+
+    ]
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
-driver.get("https://www.youtube.com/")
-time.sleep(2)
-search = driver.find_element(By.NAME, "search_query")
-search.send_keys("productivity tips")
-search.send_keys(Keys.RETURN)
-time.sleep(5)
-channels = driver.find_elements(By.CSS_SELECTOR, "ytd-channel-name a")
-creators =[]
+all_creators =[]
+seen_urls = set()
 
-for channel in channels[:10]:
-    name = channel.text
-    url = channel.get_attribute("href")
-    if name and url:
-        creators.append({"name": name, "about_url": url + "/about"})
+for niche in NICHES:
+    print(f'Searching for{niche}')
+    driver.get("https://www.youtube.com")
+    time.sleep(2)
+
+    search = driver.find_element(By.NAME,"search_query")
+    search.clear()
+    search.send_keys(niche)
+    search.send_keys(Keys.RETURN)
+    time.sleep(5)
+    channels = driver.find_elements(By.CSS_SELECTOR, "ytd-channel-name a")
+
+    for channel in channels[:10]:
+        name = channel.text
+        url = channel.get_attribute("href")
+        if name and url and url not in seen_urls:
+            seen_urls.add(url)
+            all_creators.append({"name": name, "about_url": url + "/about", "niche": niche})
+driver.quit()
+
 
 with open("creators.csv","w", newline="") as f:
-    writer = csv.DictWriter(f, fieldnames=["name","about_url"])
+    writer = csv.DictWriter(f, fieldnames=["name","about_url","niche"])
+
     writer.writeheader()
-    writer.writerows(creators)
+    writer.writerows(all_creators)
 
-print(f"Saved {len(creators)} creators to creators.csv")
+print(f"Saved {len(all_creators)} unique  creators to creators.csv")
 
-driver.quit()
+
 
 
 
